8fc72f56a3a9224e6570bc160a8ab79a2b94524b
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-11 14:59:31 +0100

    fix cors to fix error

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,9 @@ const controller = require('../controllers/auth.controller');
 module.exports = function(app) {
     app.use(function(req, res, next) {
         res.header(
+            'Content-Type', 'application/json',
+            'Accept', 'application/json',
+            'Access-Control-Allow-Origin: *',
             "Access-Control-Allow-Headers",
             "x-access-token, Origin, Content-Type, Accept"
         );
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,6 +5,9 @@ const User = require('../models/user.model');
 module.exports = function(app) {
     app.use(function(req, res, next){
         res.header(
+            'Content-Type', 'application/json',
+            'Accept', 'application/json',
+            'Access-Control-Allow-Origin: *',
             "Access-Control-Allow-Headers",
             "x-access-token, Origin, Content-Type, Accept"
         );
