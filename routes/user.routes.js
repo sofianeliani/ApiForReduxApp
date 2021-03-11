9d1218b727e0b0ec1388d89afab8ce77cb573d58
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-11 18:09:41 +0100

    fix cors to fix error

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,10 +4,6 @@ const User = require('../models/user.model');
 
 module.exports = function(app) {
     app.use(function(req, res, next){
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
         next();
     });
 
