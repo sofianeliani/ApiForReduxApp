f797bd92903a41d688f936d86b89dd4671e11d13
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-11 16:43:25 +0100

    fix cors to fix error

--- server.js
+++ server.js
@@ -49,6 +49,15 @@ try {
     });
   }
 
+  // prevent CORS problems
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+})
+
 /* use middleware */
 app.use(cors({credentials: true, origin: true}));
 app.use(bodyParser.json());
