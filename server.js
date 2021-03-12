5609c887f7f2390c8551266b1367085382f8bba8
Sofiane LIANI <sofiane.liani@ecole-isitech.fr>
2021-03-12 15:33:41 +0100

    cors fix header

--- server.js
+++ server.js
@@ -56,12 +56,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-// app.use(function(req, res ,next)  {
-//     res.header('Access-Control-Allow-Origin', 'http://18.192.215.62/');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.options('*', cors());
-//     next();
-// })
+app.use(function(req, res ,next)  {
+    res.header('Access-Control-Allow-Origin', 'http://18.192.215.62/');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.options('*', cors());
+    next();
+})
 
 /* first route */
 app.get('/', (req, res) => res.send({ message: "Hello"}))
